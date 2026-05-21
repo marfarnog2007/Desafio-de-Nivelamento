5e2010be1172b8fb69ae1f7bfa6c5dc167d75d96
marfarnog2007 <202501001203@alunos.ibmec.edu.br>
2026-05-21 01:59:45 -0300

    Delete Calculadora directory

--- Calculadora/main.py
+++
@@ -1,9 +0,0 @@
-from agent import executar_agente
-
-if __name__ == "__main__":
-    pergunta = input("Digite sua pergunta: ")
-    resposta = executar_agente(pergunta)
-    print("\nResposta final:")
-    print(resposta)
-
-    
